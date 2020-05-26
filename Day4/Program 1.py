0351cf96ad96c5d94844d80914d39639c5ea2d36
abhijeet4349 <65857732+abhijeet4349@users.noreply.github.com>
2020-05-26 21:36:50 +0530

    Create Program 1.py

---
+++ Day4/Program 1.py
@@ -0,0 +1,20 @@
+def Count(tup, en): 
+
+    return tup.count(en) 
+
+  
+
+
+tup = (10, 8, 5, 2, 10, 15, 10, 8, 5, 8, 8, 2) 
+
+enq = 4
+
+enq1 = 10
+
+enq2 = 8
+
+print(Count(tup, enq), "times") 
+
+print(Count(tup, enq1), "times") 
+
+print(Count(tup, enq2), "times") 

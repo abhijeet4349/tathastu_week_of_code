3ae74d8ccfdc2ab1b3646a31faaa3e289872f1c8
abhijeet4349 <65857732+abhijeet4349@users.noreply.github.com>
2020-05-27 09:16:59 +0530

    Update Program 1.py

--- Day4/Program 1.py
+++ Day4/Program 1.py
@@ -1,20 +1,9 @@
-def Count(tup, en): 
-
-    return tup.count(en) 
-
-  
-
-
-tup = (10, 8, 5, 2, 10, 15, 10, 8, 5, 8, 8, 2) 
-
-enq = 4
-
-enq1 = 10
-
-enq2 = 8
-
-print(Count(tup, enq), "times") 
-
-print(Count(tup, enq1), "times") 
-
-print(Count(tup, enq2), "times") 
+s=input("Enter number seperated by space")
+l=s.split()
+t=tuple(l)
+x=input("Enter number to find occurence")
+count=0
+for ele in t:
+	if ele==x:
+	   count=count+1
+print(count)
